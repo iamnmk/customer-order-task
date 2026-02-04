be807615eadfeafb766f56706d01ca077032058e
Numaan <numaanmk.nk@gmail.com>
2026-02-04 13:30:25 +0300

    fixed toast

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { CustomersComponent } from './pages/customers/customers';
-import { OrdersComponent } from './pages/orders/orders';
+import { CustomersComponent } from './components/customers/customers';
+import { OrdersComponent } from './components/orders/orders';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'customers', pathMatch: 'full' },
